bc822dd8ec8a8d5aedf1c849e01d3cbc68761c1b
Rubem Puttini <rubem.staciariniputtini@gmail.com>
2023-04-11 17:51:16 +0100

    Finish the first draft of the project

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,21 @@ import {
   TopAgent
 } from "components"
 
-const home = () => {
+const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: "properties",
+    config: {
+      pagination: {
+        pageSize: 4
+      }
+    }
+  })
+
+  const latestProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>
+  if (isError) return <Typography>Error</Typography>
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142D">
@@ -47,8 +61,32 @@ const home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+      <Box
+        flex={1}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#FCFCFC"
+        display="flex"
+        flexDirection="column"
+        minWidth="100%"
+        mt="25px"
+      >
+        <Typography fontSize="18px" fontWeight={600} color="#11142d">Últimos posts</Typography>
+        <Box mt={2.5} sx={{ display: "flex", flexWrap: "wrap", gap: 4}}>
+          {latestProperties.map((property) => (
+            <PropertyCard 
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              location={property.location}
+              price={property.price}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   )
 }
 
-export default home
+export default Home
