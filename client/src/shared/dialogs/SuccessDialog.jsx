101fb0d8e12262dd3922a6924ce7193350734441
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-07-12 13:10:25 +0100

    bug: add createPortal to success dialog to correct show modal background

--- client/src/shared/dialogs/SuccessDialog.jsx
+++ client/src/shared/dialogs/SuccessDialog.jsx
@@ -1,14 +1,18 @@
+import ReactDOM from "react-dom";
 import DialogButton from "shared/buttons/DialogButton";
 
-const SuccessDialog = ({ message, onClose }) => (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-70 flex justify-center items-center z-50">
-        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center mx-4 sm:mx-8">
-            <p className="text-lg font-semibold text-gray-800">{message}</p>
-            <div className="mt-6 flex justify-center">
-                <DialogButton onClick={onClose}>FECHAR</DialogButton>
+const SuccessDialog = ({ message, onClose }) => {
+    return ReactDOM.createPortal(
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-70 flex justify-center items-center z-[9999]">
+            <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center mx-4 sm:mx-8">
+                <p className="text-lg font-semibold text-gray-800">{message}</p>
+                <div className="mt-6 flex justify-center">
+                    <DialogButton onClick={onClose}>FECHAR</DialogButton>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        </div>,
+        document.body
+    );
+};
 
 export default SuccessDialog;
